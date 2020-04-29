2c2ead1a180c2bec6d65400fd06aa57c484c22bc
phara0hcom <t.elsay3d@gmail.com>
2020-04-29 13:49:36 +0900

    Web views Done

--- src/components/MailTable/MailTable.tsx
+++ src/components/MailTable/MailTable.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import defaultClasses from './MailTable.module.scss';
+import React, { useState } from 'react';
 
-import logoImg from '../../assets/images/logo.png';
 import MaxLengthCell from './MaxLengthCell';
 import ToCell from './ToCell';
 import DateCell from './DateCell';
 
+import logoImg from '../../assets/images/logo.png';
+import { ReactComponent as ArrowIcon } from '../../assets/images/icon_arrow01.svg';
+
+import defaultClasses from './MailTable.module.scss';
+
 export type MailObj = { from: string; to: string[]; subject: string; date: number };
 
 export type MailArr = Array<MailObj>;
+
+type SortTypes = 'from' | 'to' | 'subject' | 'date';
 
 export interface MailTableProps {
   mails: MailArr | [];
 }
 
 const MailTable: React.SFC<MailTableProps> = (props) => {
+  const [sortBy, setSortBy] = useState('date');
+  const [sortByDir, setSortByDir] = useState('des');
   const { mails } = props;
   const {
-    header,
-    headerItem,
     table,
+    selected,
+    arrowDown,
+    arrowUp,
     status,
     emptyMails,
     statusNumber,
@@ -31,19 +39,48 @@ const MailTable: React.SFC<MailTableProps> = (props) => {
     dateCell
   } = defaultClasses;
 
+  const onSortBy = (sortHeader: SortTypes) => () => {
+    if (sortBy === sortHeader) {
+      setSortByDir(sortByDir === 'dec' ? 'ase' : 'dec');
+    } else {
+      setSortBy(sortHeader);
+      setSortByDir('dec');
+    }
+  };
+
+  const sortArrowClass = sortByDir === 'dec' ? arrowDown : arrowUp;
+  const sortArrow = <ArrowIcon className={sortArrowClass} />;
+
   return (
     <div className={tableSection}>
-      <div className={status}>
+      <div className={['sectionWithP', status].join(' ')}>
         Results: <span className={statusNumber}>{mails.length}</span>mail(s)
       </div>
       {mails.length > 0 ? (
         <table className={table}>
           <thead>
-            <tr className={header}>
-              <th className={headerItem}>From</th>
-              <th className={headerItem}>To</th>
-              <th className={headerItem}>Subject</th>
-              <th className={headerItem}>Date</th>
+            <tr>
+              <th
+                onClick={onSortBy('from')}
+                {...(sortBy === 'from' ? { className: selected } : {})}
+              >
+                From {sortBy === 'from' ? sortArrow : null}
+              </th>
+              <th onClick={onSortBy('to')} {...(sortBy === 'to' ? { className: selected } : {})}>
+                To {sortBy === 'to' ? sortArrow : null}
+              </th>
+              <th
+                onClick={onSortBy('subject')}
+                {...(sortBy === 'subject' ? { className: selected } : {})}
+              >
+                Subject {sortBy === 'subject' ? sortArrow : null}
+              </th>
+              <th
+                onClick={onSortBy('date')}
+                {...(sortBy === 'date' ? { className: selected } : {})}
+              >
+                Date {sortBy === 'date' ? sortArrow : null}
+              </th>
             </tr>
           </thead>
           <tbody>
--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import DateSearchBar from '../components/DateSearchBar/DateSearchBar';
 
-import './App.css';
+import './App.scss';
 import MailTable from '../components/MailTable/MailTable';
 import { mockMails } from '../mock/mails';
 
 function App() {
   return (
     <div className='App'>
-      <header className='section'>
+      <header className='section sectionWithP'>
         <DateSearchBar section='left' />
       </header>
       <section className='sectionMail'>
